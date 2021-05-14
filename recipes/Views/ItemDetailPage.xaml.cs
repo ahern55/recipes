16098552688269ec17b65a5499dc5f9060790ac7
ahern55 <ahern.55@osu.edu>
2021-05-14 19:44:55 -0400

    initial commit

---
+++ recipes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using recipes.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace recipes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ recipes/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using recipes.Models;
+using recipes.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace recipes.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
