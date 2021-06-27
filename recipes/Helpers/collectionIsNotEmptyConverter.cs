a77b6744761eaf703906c1aed90eea43acb32ee5
ahern55 <ahern.55@osu.edu>
2021-06-27 12:35:07 -0400

    empty view messages!

---
+++ recipes/Helpers/collectionIsNotEmptyConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace recipes.Helpers
+{
+    public class collectionIsNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = (Collection<object>)value;
+            return collection.Count > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+    }
+}
