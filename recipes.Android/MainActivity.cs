f3572b7d4dbb4db8ad26418475a7ddc165819908
ahern55 <ahern.55@osu.edu>
2021-07-07 20:18:52 -0400

    good enough

--- recipes.Android/MainActivity.cs
+++ recipes.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.Runtime;
 
 namespace recipes.Droid
 {
-    [Activity(Label = "recipes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "recipes", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ recipes.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using AndroidX.AppCompat.App;
+using recipes.Droid;
+
+namespace com.xamarin.sample.splashscreen
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { StartActivity(new Intent(Application.Context, typeof(MainActivity))); });
+            startupWork.Start();
+        }
+
+        // Prevent the back button from canceling the startup process
+        public override void OnBackPressed() { }
+    }
+}
